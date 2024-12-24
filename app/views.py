c00c448d3abb30363cf35a5dbdf9364dd0932406
Thanhtruc2411 <trucntt2152@ut.edu.vn>
2024-12-25 00:00:18 +0700

    second

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+<<<<<<< HEAD
 
 def home(request):
     return render(request,'app/base.html')
 
+=======
+def home(request)
+    return render(request, "app/home.html")
+>>>>>>> e0c82fd (second)
--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+<<<<<<< HEAD
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webbanhang.settings")
+=======
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webbanhangon.settings")
+>>>>>>> e0c82fd (second)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
